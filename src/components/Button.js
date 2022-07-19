1c4496f6acce27c79cf4bc2ec2e8b3ca8f2d67e1
André Leal <57924518+AndreLeal950@users.noreply.github.com>
2022-07-19 18:29:11 -0300

    Finalização do clone da calculadora do iphone, para pratica de conhecimentos

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,7 +6,9 @@ import '../styles/components/button.sass'
 
 const Button = () => {
 
-    const [num, setNum] = useState(0)
+  const [num, setNum] = useState(0)
+  const [oldnum, setOldNum] = useState()
+  const [operator, setOperator] = useState()
     
   
   function inputNum(e) {
@@ -32,13 +34,32 @@ const Button = () => {
         if (num > 0) {
             setNum(-num)
         } else {
-            setNum(+num)
+            setNum(Math.abs(num))
         }
+  }
 
+  function operateHandle(e) {
+    let operatorInput = e.target.value
+    setOperator(operatorInput)
+    setOldNum(num)
+    setNum(0)
+    
+  }
+  
+  function calculate() {
+    if (operator === "/") {
 
-        
+        setNum(oldnum / num)
+    } else if (operator === "*") {
+
+        setNum(oldnum * num)     
+    } else if (operator === "-") {
+      
+        setNum(oldnum - num)
+    } else {
+        setNum(parseFloat(oldnum) + parseFloat)
     }
-
+  }
     return (
       <>
     <div className='button' >   
@@ -48,22 +69,22 @@ const Button = () => {
       <button className='gray' onClick={clear} >AC</button>
       <button className='gray' onClick={chargeSign}>+/-</button>
       <button className='gray' onClick={porcentagem} value='%' >%</button>
-      <button  className='orange' onClick={inputNum} value='/'>/</button>
+      <button  className='orange' onClick={operateHandle} value='/'>/</button>
       <button id='button' onClick={inputNum} value='7'>7</button>
       <button id='button' onClick={inputNum} value='8'>8</button>
       <button id='button' onClick={inputNum} value='9'>9</button>
-      <button className='orange' >X</button>
+      <button className='orange' onClick={operateHandle} value='*'>X</button>
       <button id='button' onClick={inputNum} value='4'>4</button>
       <button id='button' onClick={inputNum} value='5'>5</button>
       <button id='button' onClick={inputNum} value='6'>6</button>
-      <button className='orange'>-</button>
+      <button className='orange' onClick={operateHandle} value='-'>-</button>
       <button id='button' onClick={inputNum} value='1'>1</button>
       <button id='button' onClick={inputNum} value='2'>2</button>
       <button id='button' onClick={inputNum} value='3'>3</button>
-      <button className='orange' onClick={inputNum} value='+'>+</button>
+      <button className='orange' onClick={operateHandle} value='+'>+</button>
       <button className='zero' onClick={inputNum} value='0'>0</button>
       <button id='button' onClick={inputNum} value='.'>,</button>
-      <button className='orange' onClick={inputNum} value='='>=</button>
+      <button className='orange' onClick={calculate} value='='>=</button>
     </div>
   </>    
   )
