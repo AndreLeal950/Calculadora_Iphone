b305dc15cc3d3c203b443ad63d0bc3d22aa78535
André Leal <57924518+AndreLeal950@users.noreply.github.com>
2022-07-19 17:58:33 -0300

    Configuração das funcionalidade dos botões de AC, +/-, % e também da captura dos valores numéricos

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,31 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+
+import '../styles/components/button.sass'
+
 
 const Button = () => {
+
+    const [num, setNum] = useState(0)
+    
+  
+  function inputNum(e) {
+    const input = (e.target.value)
+    if (num === 0) {
+      
+        setNum(input)
+
+    } else {
+            setNum(num + input)
+    }
+    }
+    
+    function clear() {
+        setNum(0)
+    }
+
+    function porcentagem() {
+        setNum( num / 100)
+    }
+
+    function chargeSign() {
+        if (num > 0) {
+            setNum(-num)
+        } else {
+            setNum(+num)
+        }
+
+
+        
+    }
+
     return (
       <>
     <div className='button' >   
         
-        <h2 className='display'>0</h2>
+      <h2 className='display'>{ num }</h2>
         
-      <button className='gray' >AC</button>
-      <button className='gray'>+/-</button>
-      <button className='gray' >%</button>
-      <button  className='orange'>/</button>
-      <button id='button'>7</button>
-      <button id='button'>8</button>
-      <button id='button' >9</button>
+      <button className='gray' onClick={clear} >AC</button>
+      <button className='gray' onClick={chargeSign}>+/-</button>
+      <button className='gray' onClick={porcentagem} value='%' >%</button>
+      <button  className='orange' onClick={inputNum} value='/'>/</button>
+      <button id='button' onClick={inputNum} value='7'>7</button>
+      <button id='button' onClick={inputNum} value='8'>8</button>
+      <button id='button' onClick={inputNum} value='9'>9</button>
       <button className='orange' >X</button>
-      <button id='button' >4</button>
-      <button id='button' >5</button>
-      <button id='button' >6</button>
+      <button id='button' onClick={inputNum} value='4'>4</button>
+      <button id='button' onClick={inputNum} value='5'>5</button>
+      <button id='button' onClick={inputNum} value='6'>6</button>
       <button className='orange'>-</button>
-      <button id='button' >1</button>
-      <button id='button' >2</button>
-      <button id='button' >3</button>
-      <button className='orange' >+</button>
-      <button className='zero' >0</button>
-      <button id='button' >,</button>
-      <button className='orange' >=</button>
+      <button id='button' onClick={inputNum} value='1'>1</button>
+      <button id='button' onClick={inputNum} value='2'>2</button>
+      <button id='button' onClick={inputNum} value='3'>3</button>
+      <button className='orange' onClick={inputNum} value='+'>+</button>
+      <button className='zero' onClick={inputNum} value='0'>0</button>
+      <button id='button' onClick={inputNum} value='.'>,</button>
+      <button className='orange' onClick={inputNum} value='='>=</button>
     </div>
   </>    
   )
